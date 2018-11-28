64034335897dee71f40073395aca658ff1a34823
suarez96 <csuarez@unb.ca>
2018-11-28 15:17:40 -0400

    fixed choose profile pic page

--- app/src/main/java/com/example/augus/unbdates/HomePage.java
+++ app/src/main/java/com/example/augus/unbdates/HomePage.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
     @Override
@@ -15,7 +16,11 @@ public class HomePage extends AppCompatActivity {
 
         ImageView profilePic = (ImageView)findViewById(R.id.profilePic);
 //        if(user!=null FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl()
-        profilePic.setImageURI(Uri.parse("res/demo.jpg"));
+        profilePic.setImageURI(Uri.parse("@drawable/demo.jpg"));
+
+        TextView bioTextView = (TextView) findViewById(R.id.bioInfo);
+        bioTextView.setText("Live life to the fullest\nFreddy Born and Raised\nXOXO\nUNB class of 2020");
+
     }
 
     public void logOutAction(View view){
